fd14ee585764b2f4703bf25d4e2876112ee65fd6
Ronja Sjögren <ronjasjogren@hotmail.com>
2024-04-22 21:27:44 +0200

    add logic to add a plane

--- ControlTower.cs
+++ ControlTower.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace AirportSimulator
 {
-    internal class ControlTower
+    public class ControlTower
     {
+        private List<Airplane> airplanes = new List<Airplane>();
+        private ListBox listBox;
+
+        public ControlTower(ListBox listBox)
+        {
+            this.listBox = listBox;
+        }
+        public List<Airplane> Airplanes { get => airplanes; set => airplanes = value; }
+
+        public void AddAirplane(Airplane airplane)
+        {
+            airplanes?.Add(airplane);
+        }
+
+        public void OrderLanding(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OrderTakeOff(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OrderChangeAltitude(int index, int altitude)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnDisplayInfo(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,9 +16,31 @@ namespace AirportSimulator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ControlTower controlTower;
+
         public MainWindow()
         {
             InitializeComponent();
+            controlTower = new ControlTower(lstPlanes);
+        }
+        private void UpdateGUI()
+        {
+            FillListBox();
+        }
+        private void FillListBox()
+        {
+            lstPlanes.Items.Clear();
+            foreach (Airplane airplane in controlTower.Airplanes)
+            {
+                lstPlanes.Items.Add(airplane);
+            }
+        }
+        private void ClearTextBoxes()
+        {
+            txtName.Text = "";
+            txtFlightID.Text = "";
+            txtDestination.Text = "";
+            txtFlightTime.Text = "";
         }
         private void btnTakeOff_Click(object sender, RoutedEventArgs e)
         {
@@ -26,11 +48,38 @@ namespace AirportSimulator
         }
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
-
+            Airplane? airplane = ReadAndSavePlaneInfo();
+            if (airplane != null)
+            {
+                controlTower.AddAirplane(airplane);
+                ClearTextBoxes();
+            }
+            UpdateGUI();
         }
+
+
         private void btnChangeAltitude_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private Airplane? ReadAndSavePlaneInfo()
+        {
+            if (
+                string.IsNullOrEmpty(txtName.Text) ||
+                string.IsNullOrEmpty(txtFlightID.Text) ||
+                string.IsNullOrEmpty(txtDestination.Text)||
+                !double.TryParse(txtFlightTime.Text, out double result) || result <= 0
+                )
+            {
+                MessageBox.Show("Please fill in all the fields");
+                return null;
+            } 
+            else
+            {
+                Airplane airplane = new Airplane(txtName.Text, txtFlightID.Text, txtDestination.Text, double.Parse(txtFlightTime.Text));
+                return airplane;
+            }
         }
     }
 }
